c5bdba6ec2cb1e3481b2aed5d8aa9fa09f13c660
Ricardo-Lozano3 <66384385+Ricardo-Lozano3@users.noreply.github.com>
2021-06-10 21:47:51 -0500

    Add files via upload

---
+++ registros/registros.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { emailPattern, nombreApellidoPattern, validarUsername } from 'src/app/shared/validators/validators';
+import { ValidatorsService } from 'src/app/shared/validators/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
+
+@Component({
+  selector: 'app-registros',
+  templateUrl: './registros.component.html',
+  styles: [
+  ]
+})
+export class RegistrosComponent {
+
+  constructor( private formBuilder: FormBuilder,
+               private validatorService: ValidatorsService,
+               private emailValidator: EmailValidatorService ) { }
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ '', [Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern )] ],
+    email: [ '', [Validators.required, Validators.pattern( this.validatorService.emailPattern )], [this.emailValidator] ],
+    username: [ '', [Validators.required, this.validatorService.validarUsername] ],
+    contraseña: [ '', [Validators.required, Validators.minLength(6)] ],
+    fecha: [ '', Validators.required ],
+    confirmar: [ '', [Validators.required] ],
+  }, {
+    validators: [ this.validatorService.camposIguales( 'contraseña', 'confirmar' ) ]
+  })
+
+  get emailErrorMsg(){
+
+    const erros = this.miFormulario.get('email')?.errors
+    if ( erros?.required ){
+      return 'El correo es obligatorio'
+    }else if ( erros?.pattern ){
+      return 'El valor no tiene forma de correo'
+    }else if ( erros?.emailTomado ){
+      return 'El correo ya existe'
+    }
+
+    return ''
+
+  }
+
+  campoInvalido( campo: string ){
+    return this.miFormulario.get( campo )?.invalid && this.miFormulario.get(campo)?.touched
+  }
+
+  verFecha(){
+    let fecha = this.miFormulario.get('fecha')?.value
+    console.log(fecha)
+  }
+
+  crearFormulario(){
+
+    console.log(this.miFormulario.value)
+
+    this.miFormulario.markAllAsTouched()
+  }
+
+}
